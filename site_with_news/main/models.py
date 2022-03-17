de749c286ff2684666a5bba4e48590e7ec36ffed
NoNamePL <rdymshakov@inbox.ru>
2022-03-17 22:35:13 +0300

    Update models.py

--- site_with_news/main/models.py
+++ site_with_news/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField("Название", max_length=50)
+    text = models.TextField("Описание",)
+
+    def __str__(self):
+        return self.title
