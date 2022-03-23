817c82d3c7569029a09e182b0971e86948f9f538
NoNamePL <rdymshakov@inbox.ru>
2022-03-23 19:41:44 +0300

    normal version

--- homework/block_1/2.py
+++ homework/block_1/2.py
@@ -4,16 +4,17 @@ if "С" in input_string or 'c' in input_string:
     print("Буква 'С' или 'с' есть в введённой строке")
 #elif 'с' in input_string:
 #    print("Буква 'с' есть в введённой строке")
+counter = 0
+cout_string = len(input_string)
 
 for letter in input_string:
+    counter += 1
     if len(input_string) > 3:
-        if letter == input_string[2]:
+        if counter == 3:
             continue
 
-    if letter == input_string[-1]:
+    if counter == cout_string:
         break
     print(letter, end="")
-
-
-cout_letter = len(input_string)
-print("\nКоличество букв:", cout_letter)
+    
+print("\nКоличество букв:", cout_string)
